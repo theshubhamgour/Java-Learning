29f466ef453ebb43b437ddadec65e68e73c5dddb
Shubham Gour <72512204+theshubhamgour@users.noreply.github.com>
2023-05-05 20:23:53 +0530

    Program #4

---
+++ PrimeNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+        
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
