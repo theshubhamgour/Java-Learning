a91ee17b9755bca031effc176d2b51f7728e0164
Shubham Gour <72512204+theshubhamgour@users.noreply.github.com>
2023-05-06 13:22:39 +0530

    Program #12

---
+++ ReverseNumberDemo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseNumberDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("The reversed number is " + reverse);
+    }
+}
