764f87dff370e5988dcec38b3bb3fd5ce4702e06
theshubhamgour <iamtheshubhamgour@gmail.com>
2023-09-28 12:39:12 +0530

    Added

--- JavaBasics/VarsDB.java
+++ JavaBasics/VarsDB.java
@@ -3,6 +3,15 @@ package JavaBasics;
 public class VarsDB {
     public static void main(String[] args) {
         int a = 4;
+        String name = "Shubham";
+        double num = 3.45;
+        char c = 's';
+        boolean b = false;
         System.out.println("The value of A is : "+a);
+        System.out.println("The Sum is : "+(a+2));
+        System.out.println("The name is : "+name);
+        System.out.println(num);
+        System.out.println(c);
+        System.out.println(b);
     }
 }
